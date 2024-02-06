3650ea43d17a5f1ba10fda2544d9de361c0732ff
Gordon Ferguson <gordon@gordonferguson.me>
2024-02-05 21:06:48 -0500

    Introduced text objects

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,6 +22,24 @@ require("lazy").setup({
     },
     {
         "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "bash", "c", "fish", "lua", "javascript", "python", "query" ,"vim", "vimdoc" },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                      init_selection = "<Leader>ss",
+                      node_incremental = "<Leader>si",
+                      scope_incremental = "<Leader>sc",
+                      node_decremental = "<Leader>sd",
+                    },
+                  },
+            })
+        end,
         dependencies = {
             "nvim-treesitter/nvim-treesitter-textobjects",
         },
