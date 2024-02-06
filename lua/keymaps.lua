ee66f453cdf694fd8d0b5f3dde1256afbca2d364
Gordon Ferguson <gordon@gordonferguson.me>
2024-02-05 23:06:00 -0500

    Telescope functionality

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -44,3 +44,26 @@ keymap.set('n', '<Leader>nj', '<Cmd>Neorg journal<CR>', { silent = true, desc = 
 keymap.set('n', '<Leader>nr', '<Cmd>Neorg return<CR>', { silent = true, desc = 'Neorg Return' })
 keymap.set('n', '<Leader>nwp', '<Cmd>Neorg workspace Personal<CR>', { silent = true, desc = 'Personal Neorg' })
 keymap.set('n', '<Leader>nww', '<Cmd>Neorg workspace Work<CR>', { silent = true, desc = 'Work Neorg' })
+
+-- Telescope
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
+-- See `:help telescope.builtin`
+keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
