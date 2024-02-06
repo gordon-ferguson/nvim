0df69af827f8c688b4708a6ce73a42c8b578a6c7
Gordon Ferguson <gordon@gordonferguson.me>
2024-02-05 20:26:53 -0500

    Added some options

--- lua/options.lua
+++ lua/options.lua
@@ -1,23 +1,41 @@
+-- Numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Splits
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
+-- Wrap
 vim.opt.wrap = false
 
+-- Indentation
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
+-- Clipboard
 vim.opt.clipboard = "unnamedplus"
 
+-- Window Adjustments
 vim.opt.scrolloff = 999
+vim.opt.virtualedit = "block"
 
 vim.opt.ignorecase = true
 
-vim.opt.virtualedit = "block"
-
 vim.opt.inccommand = "split"
 
+-- Colors   
 vim.opt.termguicolors = true
+
+-- Swap
+vim.opt.swapfile = false
+
+-- Completion menu transparency
+vim.opt.pumblend = 10
+
+-- Folds / Neorg
+vim.opt.conceallevel = 1
+
+-- Show pending command in normal mode
+vim.opt.showcmd = true
