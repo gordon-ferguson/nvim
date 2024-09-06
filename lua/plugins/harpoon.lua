295ddbd60eebd55c18925373913b73d5f5f7d090
Gordon Ferguson <gordon@gordonferguson.me>
2024-09-06 10:06:58 -0400

    Changed invocation of harpoon from leader to localleader.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -9,23 +9,23 @@ return {
 
         harpoon:setup({
 
-            vim.keymap.set("n", "<leader>ha", function()
+            vim.keymap.set("n", "<LocalLeader>ha", function()
                 harpoon:list():append()
             end, { desc = "Add current file to harpoon" }),
-            vim.keymap.set("n", "<leader>hh", function()
+            vim.keymap.set("n", "<LocalLeader>hh", function()
                 harpoon.ui:toggle_quick_menu(harpoon:list())
             end, { desc = "Toggle Harpoon Menu" }),
 
-            vim.keymap.set("n", "<leader>hi", function()
+            vim.keymap.set("n", "<LocalLeader>hi", function()
                 harpoon:list():select(1)
             end, { desc = "Select first item in harpoon" }),
-            vim.keymap.set("n", "<leader>hj", function()
+            vim.keymap.set("n", "<LocalLeader>hj", function()
                 harpoon:list():select(2)
             end, { desc = "Select second item in harpoon" }),
-            vim.keymap.set("n", "<leader>hk", function()
+            vim.keymap.set("n", "<LocalLeader>hk", function()
                 harpoon:list():select(3)
             end, { desc = "Select third item in harpoon" }),
-            vim.keymap.set("n", "<leader>hl", function()
+            vim.keymap.set("n", "<LocalLeader>hl", function()
                 harpoon:list():select(4)
             end, { desc = "Select fourth item in harpoon" }),
         })
