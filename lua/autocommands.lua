7d1d5bc5f40e2e44a9ccb4228ecdb241727dcb8b
Gordon <41702924+gordon-ferguson@users.noreply.github.com>
2024-10-31 12:46:08 -0400

    Update autocommands.lua

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -5,5 +5,21 @@ return {
         vim.cmd('new')
         vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
         vim.opt_local.modified = false
-    end, { nargs = '+', complete = 'command' })
+    end, { nargs = '+', complete = 'command' }),
+
+    vim.api.nvim_create_autocmd("BufNewFile", {
+      pattern = "*.nix",
+      callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+      end,
+    }),
+    
+    vim.api.nvim_create_autocmd("BufRead", {
+      pattern = "*.nix",
+      callback = function()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.shiftwidth = 2
+      end,
+    }),
 }
